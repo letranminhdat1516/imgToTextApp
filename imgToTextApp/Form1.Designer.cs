ba38a1201dee1d78681604ed2d091eb762cd1322
Cicca <Cicca@CICCA>
2023-09-14 17:37:06 +0700

    Add project files.

---
+++ imgToTextApp/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace imgToTextApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.btnSelectFile = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtDisplayPath = new System.Windows.Forms.TextBox();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.AllowDrop = true;
+            this.btnConvert.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnConvert.Location = new System.Drawing.Point(25, 112);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(608, 49);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert \r\n";
+            this.btnConvert.UseVisualStyleBackColor = false;
+            this.btnConvert.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSelectFolder.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelectFolder.Location = new System.Drawing.Point(189, 25);
+            this.btnSelectFolder.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(165, 28);
+            this.btnSelectFolder.TabIndex = 1;
+            this.btnSelectFolder.Text = "Select Folder";
+            this.btnSelectFolder.UseVisualStyleBackColor = false;
+            this.btnSelectFolder.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btnSelectFile
+            // 
+            this.btnSelectFile.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btnSelectFile.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSelectFile.Location = new System.Drawing.Point(16, 25);
+            this.btnSelectFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSelectFile.Name = "btnSelectFile";
+            this.btnSelectFile.Size = new System.Drawing.Size(165, 28);
+            this.btnSelectFile.TabIndex = 2;
+            this.btnSelectFile.Text = "Select image File";
+            this.btnSelectFile.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSelectFile.UseVisualStyleBackColor = false;
+            this.btnSelectFile.Click += new System.EventHandler(this.btnSelectFile_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Info;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(341, 76);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(177, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = " Choose Save Location.";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
+            // 
+            // txtDisplayPath
+            // 
+            this.txtDisplayPath.Location = new System.Drawing.Point(363, 28);
+            this.txtDisplayPath.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtDisplayPath.Name = "txtDisplayPath";
+            this.txtDisplayPath.Size = new System.Drawing.Size(245, 22);
+            this.txtDisplayPath.TabIndex = 4;
+            this.txtDisplayPath.TextChanged += new System.EventHandler(this.txtDisplayPath_TextChanged);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtFileName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtFileName.Location = new System.Drawing.Point(25, 76);
+            this.txtFileName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(289, 22);
+            this.txtFileName.TabIndex = 5;
+            this.txtFileName.TextChanged += new System.EventHandler(this.txtFileName_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(21, 57);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Enter the file or folder name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(383, 25);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 16);
+            this.label2.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(359, 23);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(367, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "This is location selected";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 175);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtDisplayPath);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnSelectFile);
+            this.Controls.Add(this.btnSelectFolder);
+            this.Controls.Add(this.btnConvert);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.Button btnSelectFile;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtDisplayPath;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ imgToTextApp/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+namespace imgToTextApp
+{
+    public partial class Form1 : Form
+    {
+        private string selectedPath; // Đường dẫn của file hoặc folder đã chọn
+        private string saveLocation; // Nơi lưu kết quả
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        // button convert
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(saveLocation))
+            {
+                MessageBox.Show("Please select a save location first.");
+                return;
+            }
+            // process choosing the folder or file
+            // choose the path using tesseractEngine 
+            using (var engine = new TesseractEngine(@"C:\Program Files (x86)\tessdata", "eng", EngineMode.Default))
+            {
+                if (Directory.Exists(selectedPath)) // if folder
+                {
+                    string newFolderName = "textFolder" + DateTime.Now.ToString("yyyyMMddHHmmss");
+                    string newFolderPath = Path.Combine(saveLocation, newFolderName);
+                    Directory.CreateDirectory(newFolderPath);
+
+                    foreach (var imageFile in Directory.GetFiles(selectedPath, "*.png").Union(Directory.GetFiles(selectedPath, "*.jpg")))
+                    {
+                        ProcessImage(imageFile, engine, newFolderPath);
+                    }
+                }
+                else if (File.Exists(selectedPath)) //if file
+                {
+                    ProcessImage(selectedPath, engine, saveLocation);
+                }
+            }
+
+            MessageBox.Show("Conversion done!");
+        }
+        // process img  
+        // img --> txt
+        private void ProcessImage(string imageFile, TesseractEngine engine, string savePath)
+        {
+            // load file
+            using (var img = Pix.LoadFromFile(imageFile))
+            {
+                var page = engine.Process(img);
+                string textResult = page.GetText();
+
+                string saveFileName = string.IsNullOrEmpty(txtFileName.Text) ? Path.GetFileNameWithoutExtension(imageFile) : txtFileName.Text;
+                File.WriteAllText(Path.Combine(savePath, saveFileName + ".txt"), textResult);
+            }
+        }
+        // button select folder
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedPath = folderDialog.SelectedPath;
+                    txtDisplayPath.Text = selectedPath;
+                }
+            }
+        }
+        // button save location
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            using (var folderDialog = new FolderBrowserDialog())
+            {
+                if (folderDialog.ShowDialog() == DialogResult.OK)
+                {
+                    saveLocation = folderDialog.SelectedPath;
+                    MessageBox.Show("Location selected!");
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+        // button select file
+        private void btnSelectFile_Click(object sender, EventArgs e)
+        {
+            using (var fileDialog = new OpenFileDialog())
+            {
+                fileDialog.Filter = "Image Files|*.png;*.jpg";
+                if (fileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    selectedPath = fileDialog.FileName;
+                    txtDisplayPath.Text = selectedPath;
+                }
+            }
+        }
+        // text display input path -- can be change by text
+        private void txtDisplayPath_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        // text file name  -- can be change by text
+        private void txtFileName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
